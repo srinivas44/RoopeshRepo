8f82a05b0418b619899e1d7c5212c6da29faf234
roopesh <roopesh@localhost>
2024-04-20 12:47:33 +0530

    Added files java

---
+++ prg1.java
@@ -0,0 +1,14 @@
+public class Test {
+Int VAR1 =20;
+Int VAR2 = 10;
+Int x = 5;
+public static void main (String[] args)
+{
+
+Test obj1 = new Test obj1();
+Test obj2 = new Test obj2();
+System.out.println ("The value of x is" obj1.x)
+System.out.println ("The value of x is" obj2.x)
+}
+
+}
